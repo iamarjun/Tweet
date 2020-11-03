73b80f929fe416ca2a646fda3f292a13c6141385
iamarjun <arjunmanoj1995@gmail.com>
2020-11-03 16:32:45 +0530

    init timber for logging

--- app/src/main/java/com/arjun/tweet/TweetApp.kt
+++ app/src/main/java/com/arjun/tweet/TweetApp.kt
@@ -1,10 +1,13 @@
 package com.arjun.tweet
 
 import android.app.Application
+import timber.log.Timber
 
 class TweetApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
     }
 }
